e98291036837ee7130f17949fe9b13f2a2a2f0a6
Emmmanuel <adarikushie00@gmail.com>
2025-02-23 07:26:22 +0100

    update start up view

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
-import 'package:task_manager_app/ui/common/ui_helpers.dart';
+import 'package:task_manager_app/ui/constant/src/app_helpers.dart';
 
 import 'startup_viewmodel.dart';
 
--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -10,9 +10,6 @@ class StartupViewModel extends BaseViewModel {
   Future runStartupLogic() async {
     await Future.delayed(const Duration(seconds: 3));
 
-    // This is where you can make decisions on where your app should navigate when
-    // you have custom startup logic
-
-    _navigationService.replaceWithHomeView();
+    _navigationService.navigateToTaskListView();
   }
 }
