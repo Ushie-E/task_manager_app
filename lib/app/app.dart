542138471481dc5dc5cc12a47cf93f7d3c825581
Emmmanuel <adarikushie00@gmail.com>
2025-02-23 07:27:09 +0100

    update task app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,22 +1,31 @@
-import 'package:task_manager_app/ui/views/home/home_view.dart';
 import 'package:task_manager_app/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:task_manager_app/ui/bottom_sheets/alert/alert_sheet.dart';
 import 'package:task_manager_app/ui/dialogs/error/error_dialog.dart';
+import 'package:task_manager_app/services/task_service.dart';
+import 'package:task_manager_app/services/notification_service.dart';
+import 'package:task_manager_app/ui/views/task_list/task_list_view.dart';
+import 'package:task_manager_app/ui/views/task_forms/task_forms_view.dart';
 // @stacked-import
 
 @StackedApp(
   routes: [
-    MaterialRoute(page: HomeView),
-    MaterialRoute(page: StartupView),
-    // @stacked-route
+    AdaptiveRoute(page: StartupView, initial: true),
+    CustomRoute(page: TaskListView),
+    CustomRoute(
+      page: TaskFormsView,
+      path: ':task',
+    ),
+// @stacked-route
   ],
   dependencies: [
     LazySingleton(classType: BottomSheetService),
     LazySingleton(classType: DialogService),
     LazySingleton(classType: NavigationService),
-    // @stacked-service
+    LazySingleton(classType: TaskService),
+    LazySingleton(classType: NotificationService),
+// @stacked-service
   ],
   bottomsheets: [
     StackedBottomsheet(classType: AlertSheet),
