7d74fdc3e822b3e1c993f48bf08a11afa2f58196
Emmmanuel <adarikushie00@gmail.com>
2025-02-24 14:49:56 +0100

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:task_manager_app/app/app.locator.dart';
 import 'package:task_manager_app/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:task_manager_app/model/task.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  await Hive.openBox<Task>('tasks');
   await setupLocator();
   runApp(const MainApp());
 }
